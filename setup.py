2bc252fea336c48c0f83a293efff8fbac1d5d44d
Dustin Oprea <myselfasunder@gmail.com>
2014-03-13 02:54:53 -0400

    Updated setup.py .

--- setup.py
+++ setup.py
@@ -6,16 +6,20 @@ versioneer.versionfile_source = 'upstart/_version.py'
 versioneer.versionfile_build = 'upstart/_version.py'
 versioneer.tag_prefix = ''
 versioneer.parentdir_prefix = 'upstart-'
-  
+
+long_description=\
+"An intuitive library interface to Upstart for service and job management. "\
+"Requires the python-dbus Ubuntu package or equivalent."
+
 setup(name='upstart',
       version=versioneer.get_version(),
       description="Upstart-based service management.",
-      long_description="",
+      long_description=long_description,
       classifiers=[],
       keywords='upstart dbus',
       author='Dustin Oprea',
       author_email='myselfasunder@gmail.com',
-      url='',
+      url='https://github.com/dsoprea/PythonUpstart',
       license='GPL 2',
       packages=find_packages(exclude=['tests']),
       include_package_data=True,
