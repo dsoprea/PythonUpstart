0f50d9d687d80c8074b3df0128c723ed076b7e92
Dustin Oprea <myselfasunder@gmail.com>
2014-03-15 04:19:15 -0400

    Looks like we finally got Visioneer working perfectly.

--- setup.py
+++ setup.py
@@ -1,115 +1,34 @@
 from setuptools import setup, find_packages
 import sys, os
 
+from distutils.dist import Distribution
 
-class _VLazy(object):
-      """The lazy logic for Versioneer. We can't determine any version stuff 
-      when this file is first loaded because none of the other modules may yet 
-      be in place (when installing via PIP, for instance).
+
+class VersionedDistribution(Distribution):
+      """We set the versioneer components once the flow actually begins, and 
+      all of the modules are in place.
       """
 
-      def __init__(self):
-            self.__versioneer = None
+      def __init__(self, attrs):
+            import versioneer
 
-      def __init_versioneer(self):
-            if self.__versioneer is None:
-                  import versioneer
+            versioneer.versionfile_source = 'upstart/_version.py'
+            versioneer.versionfile_build = 'upstart/_version.py'
+            versioneer.tag_prefix = ''
+            versioneer.parentdir_prefix = 'upstart-'
 
-                  versioneer.versionfile_source = 'upstart/_version.py'
-                  versioneer.versionfile_build = 'upstart/_version.py'
-                  versioneer.tag_prefix = ''
-                  versioneer.parentdir_prefix = 'upstart-'
+            attrs['version'] = versioneer.get_version()
+            attrs['cmdclass'] = versioneer.get_cmdclass()
 
-                  self.__versioneer = versioneer
-
-      def get_version(self):
-            self.__init_versioneer()
-            return self.__versioneer.get_version()
-
-      def get_cmdclass(self):
-            self.__init_versioneer()
-            return self.__versioneer.get_cmdclass()
-
-      def __str__(self):
-            raise Exception("STR!")
-
-      def __repr__(self):
-            raise Exception("REPR!")
-
-class _VLazyVersion(str):
-      """Wait to determine the version until someone actually casts us or 
-      applies us, after our whole project has been staged.
-      """
-
-      def __init__(self, v):
-#            super(_VLazyVersion, self).__init__()
-
-            self.__v = v
-            self.__version = None
-
-      def __repr__(self):
-            return str(self)
-
-      def __str__(self):
-            if self.__version is None:
-                  self.__version = self.__v.get_version()
-
-            ver = self.__version
-            print("VERSION: %s" % (ver))
-            return ver
-
-class _VLazyCmdClass(object):
-      """Wait to determine the cmdclass until someone actually requests a 
-      particular class, at which point we proxy the request to the actual 
-      dictionary.
-      """
-
-      def __init__(self, v):
-#            super(_VLazyCmdClass, self).__init__()
-
-            self.__v = v
-            self.__cmdclass = None
-
-      def __init_cmdclass(self):
-            if self.__cmdclass is None:
-                  self.__cmdclass = self.__v.get_cmdclass()
-
-      def __getattr__(self, name):
-            """Proxy all requests through to the standard dictionary object. 
-            The first time we're called, load ourselves with the Versioneer 
-            cmdclass entries.
-            """
-
-            self.__init_cmdclass()
-            return getattr(self.__cmdclass, name)
-
-      def __repr__(self):
-            self.__init_cmdclass()
-            return repr(self.__cmdclass)
-
-      def __str__(self):
-            self.__init_cmdclass()
-            return str(self.__cmdclass)
-
-      def __iter__(self):
-            self.__init_cmdclass()
-            return iter(self.__cmdclass.items())
-
-      def __setitem__(self, k, v):
-            self.__init_cmdclass()
-            self.__cmdclass[k] = v
+            Distribution.__init__(self, attrs)
 
 long_description=\
 "An intuitive library interface to Upstart for service and job management. "\
 "Requires the python-dbus Ubuntu package or equivalent."
 
-v = _VLazy()
-lv = _VLazyVersion(v)
-print("LV(str): %s" % (str(lv)))
-print("LV(repr): %s" % (repr(lv)))
-
-setup(name='upstart',
-      version=lv, #versioneer.get_version(),
+setup(distclass=VersionedDistribution,
+      name='upstart',
+      version=None, #_VLazyVersion(), #versioneer.get_version(),
       description="Upstart-based service management.",
       long_description=long_description,
       classifiers=[],
@@ -123,6 +42,6 @@ setup(name='upstart',
       zip_safe=False,
       install_requires=[],
       entry_points="",
-      cmdclass=_VLazyCmdClass(v)
-#      cmdclass=versioneer.get_cmdclass(),
+      cmdclass=None,#versioneer.get_cmdclass(),
+#      cmdclass=_VLazyCmdClass()
 )
